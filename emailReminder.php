a8b1626912bc1ce99b0876e56d5b51a949f06b00
Sam H <sam.hiscox@ntlworld.com>
2017-01-05 21:41:31 +0000

    emailReminder
    Working PHP script to send emails for events that are the next day to
    the attendees. Will need a task scheduler.

--- emailReminder.php
+++ emailReminder.php
@@ -4,39 +4,23 @@ include 'Database-config.php';
 //start session
 session_start();
 
-//Login to database
-$db = new PDO("mysql:dbname=$dbname", "$dbusername", "$dbpassword");
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-//GET HUANY MIN TO WRITE SQL QUERIES FOR GETTING IST OF EMAILZ
-
+date_default_timezone_set('Etc/UTC');
 require '/PHPMailer/PHPMailerAutoload.php';
 
-//SMTP needs accurate times, and the PHP time zone MUST be set
-//This should be done in your php.ini, but this is how to do it if you don't have access to that
-date_default_timezone_set('Etc/UTC');
-//Create a new PHPMailer instance
+
 $mail = new PHPMailer;
-//Tell PHPMailer to use SMTP
 $mail->isSMTP();
 //Enable SMTP debugging
 // 0 = off (for production use)
 // 1 = client messages
 // 2 = client and server messages
 $mail->SMTPDebug = 0;
-//Ask for HTML-friendly debug output
 $mail->Debugoutput = 'html';
-//Set the hostname of the mail server
 $mail->Host = 'smtp.mail.com';
-// use
-// $mail->Host = gethostbyname('smtp.gmail.com');
-// if your network does not support SMTP over IPv6
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
 $mail->Port = 587;
-//Set the encryption system to use - ssl (deprecated) or tls
 $mail->SMTPSecure = 'tls';
-//Whether to use SMTP authentication
 $mail->SMTPAuth = true;
+$mail->SMTPKeepAlive = true; // SMTP connection will not close after each email sent, reduces SMTP overhead
 //Username to use for SMTP authentication - use full email address for gmail
 $mail->Username = "searchparty@engineer.com";
 //Password to use for SMTP authentication
@@ -45,21 +29,41 @@ $mail->Password = "COMP310P";
 $mail->setFrom('searchparty@engineer.com', 'SearchParty');
 //Set an alternative reply-to address
 $mail->addReplyTo('searchparty@engineer.com', 'SearchParty');
-//Set who the message is to be sent to
-$mail->addAddress('sam.hiscox@ntlworld.com', 'Sam Hiscox');
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML('<div> This is some plain content </div>');
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
-//Attach an image file
-//$mail->addAttachment('images/phpmailer_mini.png');
-//send the message, check for errors
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
+$mail->Subject = "Event Reminder!";
+
+//Login to database
+$db = new PDO("mysql:dbname=$dbname", "$dbusername", "$dbpassword");
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+//Get list of events where a reminder hasn't been sent and it is the day before the startdate
+$result=$db->query("SELECT Event_ID, Title FROM event WHERE Reminder_sent= 0 AND StartDate = CURDATE() - INTERVAL 1 DAY");
+//LOOP OVER EVENTS
+foreach ($result as $row) {
+	//echo $row['Event_ID'];
+	//GET LIST OF EMAILS
+	$result2=$db->query("SELECT u.Email, CONCAT(u.firstname , ' ', u.lastname) AS Name FROM user as u
+		JOIN fkguest_list AS fk ON fk.User_ID = u.User_ID 
+		WHERE fk.Event_ID = '".$row['Event_ID']."' ");
+	foreach ($result2 as $row2) {
+		// echo $row2['Email'];
+		// echo $row2['Name'];
+		//Set address
+		$mail->addAddress($row2['Email'], $row2['Name']);
+		$body = "<p>Dear '".$row2['Name']."',</p>
+		<p>This is a reminder that your event '".$row['Title']."' is tomorrow!</p>
+		<p>From SearchParty</p>";
+		$mail->msgHTML($body);
+		if(!$mail->send()){
+			echo "Sending email to '".$row2['Email']."' failed";
+			echo $mail->ErrorInfo;
+		}else{
+			$sql = "UPDATE event SET Reminder_sent=1 WHERE Event_ID = '".$row['Event_ID']."'";
+			$stmt = $db->prepare($sql);
+			$stmt->execute();
+			//echo "Sending email to '".$row2['Email']."' Success";
+		}
+	}
 }
+
 ?>
