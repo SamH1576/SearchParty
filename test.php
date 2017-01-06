ef21058cc200fbca10731a7d27071673587a2fd7
Sam H <sam.hiscox@ntlworld.com>
2017-01-06 16:53:38 +0000

    Deleted Unnecessary Files

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'Database-config.php';	
-    //Login to database
-	
-	$db = new PDO("mysql:host=$dbhost;dbname=$dbname", "$dbusername", "$dbpassword");
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-?>
