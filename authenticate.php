29d235208ae64b941cf56a9c2c2530abdf67634c
minimumness <ganhuanmin@gmail.com>
2017-01-10 22:30:53 +0000

    if user login email exists but wrong corresponding password, error message will show user exists but wrong password

--- authenticate.php
+++ authenticate.php
@@ -9,6 +9,7 @@ function check_username_password_match($array){
 	global $dbusername;
 	global $dbpassword;
     global $dbhost;
+    global $returndata;
 	
     //Login to database
 	$db = new PDO("mysql:host=$dbhost;dbname=$dbname", "$dbusername", "$dbpassword");
@@ -16,15 +17,24 @@ function check_username_password_match($array){
     $username = $array['username']; 
     $password = $array['password'];
     $result=$db->query("SELECT User_ID FROM user WHERE email= '$username' and BINARY password= '$password' ");
+    $result1=$db->query("SELECT User_ID FROM user WHERE email= '$username' ");
     if($result->rowCount() > 0){
         $rowdata = $result->fetch(PDO::FETCH_ASSOC);
         $_SESSION["usernameID"] = $rowdata['User_ID'];
         $db = null;
-        return True;
+        $returndata = "LogInSuccess";
+        return $returndata;
     }
+    else if($result1->rowCount() > 0){
+        $db = null;
+        $returndata = "EmailMatch";
+        return $returndata;
+    }
+
     else{
         $db=null;
-        return False;
+        $returndata = "LogInFail";
+        return $returndata;
     }
 }
 /*****************************************************/
@@ -32,12 +42,17 @@ function check_username_password_match($array){
 /*****************************************************/
 $verb = $_SERVER['REQUEST_METHOD'];
     if($verb == 'POST'){
-		if(check_username_password_match($_POST)){
+		if(check_username_password_match($_POST)== "LogInSuccess"){
 			$_SESSION["loggedIn"] = True;
 			$_SESSION["username"] = $_POST['username'];	
 			include 'main.html';
-    }
-        else{
+        }
+        else if(check_username_password_match($_POST)== "EmailMatch"){
+            echo "<div id = 'alertbox'> Username exists, but that password was incorrect. Please enter the correct password! </div>";
+            $_SESSION["loggedIn"] = False;
+            include 'login.html';
+        }
+        else if(check_username_password_match($_POST)== "LogInFail"){
             echo "<div id = 'alertbox'> Username or password incorrect, if you have not signed up please sign up below</div>";
 			$_SESSION["loggedIn"] = False;
             include 'login.html';
